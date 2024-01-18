7a76249106f2e09168c4972254742aea2faa1bb8
KhawajaAbdulMoiz <156238498+KhawajaAbdulMoiz@users.noreply.github.com>
2024-01-18 22:03:31 +0500

    Add files via upload

---
+++ KhawajaAbdulMoiz_midterm.py
@@ -0,0 +1,33 @@
+# Khawaja Abdul Moiz
+# MidTerm Examination 
+
+# Q1 - Write a Python program to do arithmetical operations addition and division.
+
+
+num1=(int(input('Enter first Number:')))
+num2=(int(input('Enter second number:')))
+sum(num1+num2)
+print("sum:",num1,"+",num2,"=",sum)
+
+# Q2 - Write a Python program to find the area of a triangle
+
+length_of_triangle=(int(input('Enter lenght of triangle:')))
+height_of_triangle=(int(input("enter height of triangle:")))
+print("Area of Triangle is:",1/2*length_of_triangle*height_of_triangle)
+
+# Q3 - Write a Python program to convert Celsius to Fahrenheit.
+
+Temprature_in_celsius=(int(input("Enter temprature in celsius:")))
+Temprature_in_fahrenhiet=(int(input("Enter temprature in fahrenhiet:")))
+print(Temprature_in_celsius,"degree celsius","is equal to",(9/5*Temprature_in_celsius)+32)
+
+# Q4 - Write a Python program that return all datatypes that we discussed in the class.
+
+int=9
+print("The Data Type of",int,"is",type(int))
+str="Khawaja Abdul Moiz"
+print("The Data Type of",str,"is",type(str))
+float=16.5
+print("The Data Type of",float,"is",type(float))
+bool=True
+print("The Data Type of",bool,"is",type(bool))
